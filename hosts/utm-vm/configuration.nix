ae878890f172f9161fb6b0645154871a647860b4
Dennis Smith <dennisgsmith12@gmail.com>
2025-04-22 17:15:25 -0400

    feat: add utm vm, test out hyprland on it even though it will probably suck because GPU passthrough with AVF isn't a thing

--- hosts/utm-vm/configuration.nix
+++ hosts/utm-vm/configuration.nix
@@ -20,7 +20,26 @@
     outputs.nixosModules.hyprland
   ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   networking.hostName = hostname;
+  networking.networkmanager.enable = true;
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.printing.enable = true;
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
